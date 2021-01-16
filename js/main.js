13249fdb30f95bcfa19ff5769a8671700121b8f8
zhaohaoren <zhaohaoren666@gmail.com>
2021-01-16 17:18:31 +0800

    Site updated: 2021-01-16 17:18:31

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 if (/mobile/i.test(navigator.userAgent) || /android/i.test(navigator.userAgent)) {
   document.body.classList.add('mobile')
 }
+// 代码高亮
+document.querySelectorAll('pre code').forEach((block) => {
+  hljs.highlightBlock(block);
+});
